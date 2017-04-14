541fef8815bd7295df2ddc3e70e80688f89fd569
Fathuriady <fathuriady@gmail.com>
2017-04-15 00:43:47 +0700

    Regist dan Login MySQL

--- app/src/main/java/com/shsyarbin/skts/Login.java
+++ app/src/main/java/com/shsyarbin/skts/Login.java
@@ -2,122 +2,102 @@ package com.shsyarbin.skts;
 
 import android.app.ProgressDialog;
 import android.content.Intent;
-import android.os.Bundle;
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.TextView;
-
-import com.androidquery.AQuery;
-import com.androidquery.callback.AjaxCallback;
-import com.androidquery.callback.AjaxStatus;
-
-import org.json.JSONException;
-import org.json.JSONObject;
+import android.widget.Toast;
 
 import java.util.HashMap;
-import java.util.Map;
 
-public class Login extends AppCompatActivity {
-    //deklarasi variabel
-    EditText email, password;
-    Button login;
-    TextView register;
-    AQuery aq;
+public class Login extends AppCompatActivity implements View.OnClickListener{
+
+    public static final String USER_NAME = "USERNAME";
+
+    public static final String PASSWORD = "PASSWORD";
+
+    private static final String LOGIN_URL = "http://10.0.2.2/skts/login.php";
+
+    private EditText edttxtUserName;
+    private EditText edttxtPassword;
+
+    private Button btnLogin,btnRegister;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
-//deklarasi untuk memanggil ID
-        email = (EditText) findViewById(R.id.loginemail);
-        password = (EditText) findViewById(R.id.loginPassword);
-        register = (TextView) findViewById(R.id.loginRegister);
-        login = (Button) findViewById(R.id.loginLogin);
+        edttxtUserName = (EditText) findViewById(R.id.loginemail);
+        edttxtPassword = (EditText) findViewById(R.id.loginPassword);
 
-        register.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                startActivity(new Intent(getApplicationContext(), RegisterActivity.class));
-                finish();
-            }
-        });
+        btnLogin = (Button) findViewById(R.id.loginLogin);
+        btnRegister = (Button) findViewById(R.id.loginRegister);
 
-//ketika button login di klik
-        login.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                startActivity(new Intent(getApplicationContext(), WelcomeActivity.class));
-                finish();
-            }
-        });
-
+        btnRegister.setOnClickListener(this);
+        btnLogin.setOnClickListener(this);
     }
 
-    private void CHECK() {
-        email.setError(null);
-        password.setError(null);
-        boolean cancel = false;
-        View focusView = null;
-        if (Helper.isEmpty(email)) {
-            email.setError("Email tidak boleh kosong");
-            focusView = email;
-            cancel = true;
-        } else if (!Helper.isEmailValid(email)) {
-            email.setError("Email tidak valid");
-            focusView = email;
-            cancel = true;
-        } else if (Helper.isEmpty(password)) {
-            password.setError("Password tidak boleh kosong");
-            focusView = password;
-            cancel = true;
-        } else {
-            LOGIN();
-        }
+
+    private void login(){
+        String username = edttxtUserName.getText().toString().trim();
+        String password = edttxtPassword.getText().toString().trim();
+        userLogin(username,password);
     }
 
-    private void LOGIN() {
-        String url = Helper.BASE_URL + "login.php";
-
-        Map<String, String> param = new HashMap<>();
-
-        param.put(Helper.EMAIL_USER, email.getText().toString());
-        param.put(Helper.PASSWORD_USER, password.getText().toString());
-
-        ProgressDialog pdialog = new ProgressDialog(Login.this);
-        pdialog.setCancelable(true);
-        pdialog.setMessage("Loading . . .");
-
-        aq = new AQuery(Login.this);
-
-        aq.progress(pdialog).ajax(url, param, String.class, new AjaxCallback<String>() {
+    private void userLogin(final String username, final String password){
+        class UserLoginClass extends AsyncTask<String,Void,String>{
+            ProgressDialog loading;
             @Override
-            public void callback(String url, String object, AjaxStatus status) {
-                super.callback(url, object, status);
-                if (object != null){
-                    Helper.pre("Respon API : " + object);
-                    try {
-                        //deklarasi JSON
-                        JSONObject jsonObject = new JSONObject(object);
-                        //memanggil JSON Object
-                        String result = jsonObject.getString("success");
-                        String pesan = jsonObject.getString("message");
-                        if (result.equalsIgnoreCase("true")){
-                            Helper.pesan(getApplicationContext(), pesan);
-                            startActivity(new Intent(getApplicationContext(), WelcomeActivity.class));
-                            finish();
-                        }else {
-                           Helper.pesan(getApplicationContext(), pesan);
-                        }
-                    }catch (JSONException e){
-                        e.printStackTrace();
-                    }
-                }else {
-                    Helper.pesan(getApplicationContext(), "Url salah");
+            protected void onPreExecute() {
+                super.onPreExecute();
+                loading = ProgressDialog.show(Login.this,"Please Wait",null,true,true);
+            }
+
+            @Override
+            protected void onPostExecute(String s) {
+                super.onPostExecute(s);
+                loading.dismiss();
+                if(s.equalsIgnoreCase("success")){
+                    Intent intent = new Intent(Login.this,Main2Activity.class);
+                    intent.putExtra(USER_NAME,username);
+                    startActivity(intent);
+                }else{
+                    Toast.makeText(Login.this,s,Toast.LENGTH_LONG).show();
                 }
             }
-        });
+
+            @Override
+            protected String doInBackground(String... params) {
+                HashMap<String,String> data = new HashMap<>();
+                data.put("username",params[0]);
+                data.put("password",params[1]);
+
+                RegisterUserClass ruc = new RegisterUserClass();
+
+                String result = ruc.sendPostRequest(LOGIN_URL,data);
+
+                return result;
+            }
+        }
+        UserLoginClass ulc = new UserLoginClass();
+        ulc.execute(username,password);
+    }
+
+    @Override
+    public void onClick(View v) {
+        if(v == btnLogin){
+            login();
+            startActivity(new Intent(Login.this,Main2Activity.class));
+        }
+
+        if(v == btnRegister){
+            startActivity(new Intent(Login.this,RegisterActivity.class));
+            finish();
+        }
     }
 }
--- app/src/main/java/com/shsyarbin/skts/RegisterActivity.java
+++ app/src/main/java/com/shsyarbin/skts/RegisterActivity.java
@@ -9,17 +9,11 @@ package com.shsyarbin.skts;
         import android.os.AsyncTask;
         import android.support.v7.app.AppCompatActivity;
         import android.os.Bundle;
-        import android.view.Menu;
-        import android.view.MenuItem;
         import android.view.View;
         import android.widget.Button;
         import android.widget.EditText;
         import android.widget.Toast;
-
-        import java.io.BufferedReader;
-        import java.io.InputStreamReader;
-        import java.net.HttpURLConnection;
-        import java.net.URL;
+        import java.util.HashMap;
 
 public class RegisterActivity extends AppCompatActivity implements View.OnClickListener{
 
@@ -55,6 +49,7 @@ public class RegisterActivity extends AppCompatActivity implements View.OnClickL
         }
     }
 
+
     private void registerUser() {
         String nama = edttxtNama.getText().toString().trim().toLowerCase();
         String username = edttxtUsername.getText().toString().trim().toLowerCase();
@@ -65,16 +60,15 @@ public class RegisterActivity extends AppCompatActivity implements View.OnClickL
     }
 
     private void register(String nama, String username, String password, String email) {
-        String urlSuffix = "?nama="+nama+"&username="+username+"&password="+password+"&email="+email;
         class RegisterUser extends AsyncTask<String, Void, String>{
-
             ProgressDialog loading;
+            RegisterUserClass ruc = new RegisterUserClass();
 
 
             @Override
             protected void onPreExecute() {
                 super.onPreExecute();
-                loading = ProgressDialog.show(RegisterActivity.this, "Tunggu Sebentar",null, true, true);
+                loading = ProgressDialog.show(RegisterActivity.this, "Please Wait",null, true, true);
             }
 
             @Override
@@ -86,25 +80,20 @@ public class RegisterActivity extends AppCompatActivity implements View.OnClickL
 
             @Override
             protected String doInBackground(String... params) {
-                String s = params[0];
-                BufferedReader bufferedReader = null;
-                try {
-                    URL url = new URL(REGISTER_URL+s);
-                    HttpURLConnection con = (HttpURLConnection) url.openConnection();
-                    bufferedReader = new BufferedReader(new InputStreamReader(con.getInputStream()));
 
-                    String result;
+                HashMap<String, String> data = new HashMap<String,String>();
+                data.put("nama",params[0]);
+                data.put("username",params[1]);
+                data.put("password",params[2]);
+                data.put("email",params[3]);
 
-                    result = bufferedReader.readLine();
+                String result = ruc.sendPostRequest(REGISTER_URL,data);
 
-                    return result;
-                }catch(Exception e){
-                    return null;
-                }
+                return  result;
             }
         }
 
         RegisterUser ru = new RegisterUser();
-        ru.execute(urlSuffix);
+        ru.execute(nama,username,password,email);
     }
 }
